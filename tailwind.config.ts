85bf38dc9042b7a8f44b8bf619f8eb404024a74f
fuadmhnr <fuadmuhammadn@gmail.com>
2023-08-01 21:43:10 +0700

    feat(ui): convert config to plugin tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss';
+import { shadcnPreset } from './resources/js/lib/shadcn-preset';
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: ['class'],
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.tsx',
+  ],
+  presets: [shadcnPreset],
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
+
+export default config;
